f4edc5bf788fd34b25f68e7966903e7dc5bee9a4
jonreynolburce <218437243+jonreynolburce@users.noreply.github.com.>
2026-08-28 21:56:17 +0800

    Updated the form title to "Billing System v1.0 - Customer List (J.R.)

--- BillingSystem/Program.cs
+++ BillingSystem/Program.cs
@@ -3,7 +3,7 @@ namespace BillingSystem
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application Billing System v1.0.
+        /// The main entry point for the application Billing System v2.0.
         /// </summary>
         [STAThread]
         static void Main()
