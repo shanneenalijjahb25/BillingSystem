253b2422ccbbed7f5af845bfe885139dea62f365
jonreynolburce <218437243+jonreynolburce@users.noreply.github.com.>
2026-09-12 21:04:41 +0800

    Add MySQL database setup and add login validation and authentication in LoginForm.cs

--- BillingSystem/LoginForm.Designer.cs
+++ BillingSystem/LoginForm.Designer.cs
@@ -42,84 +42,77 @@
             // 
             lblTitle.AutoSize = true;
             lblTitle.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblTitle.Location = new Point(151, 43);
-            lblTitle.Margin = new Padding(5, 0, 5, 0);
+            lblTitle.Location = new Point(93, 27);
             lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(314, 50);
+            lblTitle.Size = new Size(194, 31);
             lblTitle.TabIndex = 0;
             lblTitle.Text = "BILLING SYSTEM";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(237, 43);
-            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Location = new Point(146, 27);
             label2.Name = "label2";
-            label2.Size = new Size(0, 32);
+            label2.Size = new Size(0, 20);
             label2.TabIndex = 1;
             // 
             // lblUsername
             // 
             lblUsername.AutoSize = true;
-            lblUsername.Location = new Point(120, 125);
-            lblUsername.Margin = new Padding(5, 0, 5, 0);
+            lblUsername.Location = new Point(74, 78);
             lblUsername.Name = "lblUsername";
-            lblUsername.Size = new Size(126, 32);
+            lblUsername.Size = new Size(78, 20);
             lblUsername.TabIndex = 3;
             lblUsername.Text = "Username:";
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(120, 294);
-            btnLogin.Margin = new Padding(5, 5, 5, 5);
+            btnLogin.Location = new Point(74, 184);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(153, 46);
+            btnLogin.Size = new Size(94, 29);
             btnLogin.TabIndex = 5;
             btnLogin.Text = "Login";
             btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
             // 
             // btnCancel
             // 
-            btnCancel.Location = new Point(312, 294);
-            btnCancel.Margin = new Padding(5, 5, 5, 5);
+            btnCancel.Location = new Point(192, 184);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(153, 46);
+            btnCancel.Size = new Size(94, 29);
             btnCancel.TabIndex = 6;
             btnCancel.Text = "Cancel";
             btnCancel.UseVisualStyleBackColor = true;
             // 
             // txtUsername
             // 
-            txtUsername.Location = new Point(281, 125);
-            txtUsername.Margin = new Padding(5, 5, 5, 5);
+            txtUsername.Location = new Point(173, 78);
             txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(201, 39);
+            txtUsername.Size = new Size(125, 27);
             txtUsername.TabIndex = 7;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(281, 206);
-            txtPassword.Margin = new Padding(5, 5, 5, 5);
+            txtPassword.Location = new Point(173, 129);
             txtPassword.Name = "txtPassword";
             txtPassword.PasswordChar = '*';
-            txtPassword.Size = new Size(201, 39);
+            txtPassword.Size = new Size(125, 27);
             txtPassword.TabIndex = 8;
             // 
             // lblPassword
             // 
             lblPassword.AutoSize = true;
-            lblPassword.Location = new Point(120, 211);
-            lblPassword.Margin = new Padding(5, 0, 5, 0);
+            lblPassword.Location = new Point(74, 132);
             lblPassword.Name = "lblPassword";
-            lblPassword.Size = new Size(116, 32);
+            lblPassword.Size = new Size(73, 20);
             lblPassword.TabIndex = 9;
             lblPassword.Text = "Password:";
             // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(622, 431);
+            ClientSize = new Size(383, 269);
             Controls.Add(lblPassword);
             Controls.Add(txtPassword);
             Controls.Add(txtUsername);
@@ -129,11 +122,11 @@
             Controls.Add(label2);
             Controls.Add(lblTitle);
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            Margin = new Padding(5, 5, 5, 5);
             MinimizeBox = false;
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Billing System v1.0 – Login (A.N.)";
+            Load += LoginForm_Load;
             ResumeLayout(false);
             PerformLayout();
         }
